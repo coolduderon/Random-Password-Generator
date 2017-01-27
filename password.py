243f99fc6b3b929414db56281a81da910e01d230
coolduderon <jacobw4142@gmail.com>
2017-01-26 19:24:59 -0600

    Delete password.py

--- password.py
+++
@@ -1,7 +0,0 @@
-import string
-import random
-print("Hello, This program will help generate a secure password. Please add some symbols to the end of this password to ensure security.")
-x = (random.randint(1,10000))
-y = x*(random.randint(1,10000))
-random.choice(string.ascii_letters)
-print ("Your random password is: ",random.choice(string.ascii_letters),random.choice(string.ascii_letters),random.choice(string.ascii_letters),random.choice(string.ascii_letters),random.choice(string.ascii_letters),random.choice(string.ascii_letters),y)
